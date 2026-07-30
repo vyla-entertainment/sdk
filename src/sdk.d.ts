5feb0307ae1d6f2316d5d6f6d0f0625f7092ee82
Gavin Fox <gavingogaming@gmail.com>
2026-07-30 00:28:14 -0700

    sdk fix

--- src/sdk.d.ts
+++ src/sdk.d.ts
@@ -95,7 +95,7 @@ export default class VylaSDK {
         s?: number | null,
         e?: number | null,
         clientIP?: string | null
-    ): Promise<{allUrls: StreamResult[]}>;
+    ): Promise<StreamResult | {allUrls: StreamResult[]}>;
 
     /**
      * Get subtitles for a movie or TV episode.
